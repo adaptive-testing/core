b76f124c4e0eb556c51c1b8db26b43d8020b9716
Clay Diffrient <clay.diffrient@gmail.com>
2014-04-19 21:54:52 -0600

    Adds additional initial tests, passes over half

--- test/adaptive-tester-core_test.js
+++ test/adaptive-tester-core_test.js
@@ -2,6 +2,10 @@
 
 var adaptiveTesterCore = require('../lib/adaptive-tester-core.js');
 var Question = require('../lib/question');
+var Answer = require('../lib/answer');
+var ParameterError = require('../lib/ParameterError');
+var MultipleCorrectAnswersError = require('../lib/MultipleCorrectAnswersError');
+var Student = require('../lib/student');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -23,20 +27,13 @@ var Question = require('../lib/question');
     test.ifError(value)
 */
 
+var testAnswers = [new Answer("yes", true), new Answer("no", false) ];
+var testQuestionText = "Will this work?";
+
 exports.adaptiveTesterCore = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    //test.expect(1);
-    // tests here
-    //test.equal(adaptiveTesterCore.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  },
   'empty question length': function (test) {
     test.expect(1);
-    test.equal(adaptiveTesterCore().getTotalQuestions(), 0, 'should be zero.');
+    test.equal(adaptiveTesterCore.getTotalQuestions(), 0, 'should be zero.');
     test.done();
   },
   'a question object cannot be created without any answers': function (test) {
@@ -57,8 +54,98 @@ exports.adaptiveTesterCore = {
     test.expect(1);
     test.throws(function () {
       new Question("Will this throw an exception?", []);
-    }, Error, 'should fail for an empty answers parameter.');
+    }, ParameterError, 'should fail for an empty answers parameter.');
     test.done();
+  },
+  'a question cannot be created without a valid text parameter': function (test) {
+    test.expect(1);
+    test.throws(function () {
+      new Question (null, testAnswers)
+    }, ParameterError, 'should fail for an empty question text string.');
+    test.done();
+  },
+  'a question can be added only with an array of answers': function (test) {
+    test.expect(2);
+    test.doesNotThrow(function () {
+      new Question(testQuestionText, testAnswers, 3)
+    }, ParameterError, 'should not fail with valid input.');
+    test.throws(function () {
+      new Question(testQuestionText, ["yes", "now"], 3);
+    }, ParameterError, 'should fail for non-answer input in arrays');
+    test.done();
+  },
+  'a question fails if it does not have a number for difficulty': function (test) {
+    test.expect(3);
+    test.doesNotThrow(function () {
+      new Question(testQuestionText, testAnswers, 3);
+    }, ParameterError, 'should not fail with valid input.');
+    test.throws(function () {
+      new Question(testQuestionText, testAnswers, "hard");
+    }, ParameterError, 'should fail with a string as input.');
+    test.throws(function () {
+      new Question(testQuestionText, testAnswers, NaN);
+    }, ParameterError, 'should fail for NaN input.');
+    test.done();
+  },
+  'a question fails if the difficulty is lower than 0 or more than 10': function (test) {
+    test.expect(2);
+    test.throws(function () {
+      new Question(testQuestionText, testAnswers, -1);
+    }, RangeError, 'should fail for negative difficulties');
+    test.throws(function() {
+      new Question(testQuestionText, testAnswers, 11);
+    }, RangeError, 'should fail for difficulties over 10');
+    test.done();
+  },
+  'a question can be added into the core': function (test) {
+    test.expect(1);
+    adaptiveTesterCore.addQuestion(new Question(testQuestionText, testAnswers, 3));
+    test.equal(adaptiveTesterCore.getTotalQuestions(), 1, 'should have one question within.');
+    test.done();
+  },
+  'a non-question cannot be added to the core': function (test) {
+    test.expect(1);
+    test.throws(function () {
+      adaptiveTesterCore.addQuestion("Why?");
+    }, ParameterError, 'should fail with non-question input.');
+    test.done();
+  },
+  'a question cannot have more than one correct answer': function (test) {
+    test.expect(1);
+    var answersCopy = testAnswers.slice();
+    answersCopy.push(new Answer('It depends', true));
+    test.throws(function () {
+       new Question(testQuestionText, answersCopy, 3);
+    }, MultipleCorrectAnswersError, 'should fail with more than one correct answer.');
+    test.done();
+  },
+  'a student can be created with an initial ability': function (test) {
+    test.expect(2);
+    var student = new Student(3);
+    test.ok(student, 'should create a student without problem.');
+    test.equals(student.getAbility(), 3, 'ability should be equal after creation.');
+    test.done();
+  },
+  'a student can be created with only a name': function (test) {
+    test.expect(2);
+    var student = new Student("Clay");
+    test.ok(student, 'should create a student without problem.');
+    test.equals(student.getName(), "Clay", 'name should be equal after creation.');
+    test.done();
+  },
+  'a student can be created with both a name and an initial ability': function (test) {
+    test.expect(3);
+    var student = new Student("Clay", 3);
+    test.ok(student, 'should create a student without problem.');
+    test.equals(student.getName(), "Clay", 'name should be equal after creation.');
+    test.equals(student.getAbility(), 3, 'ability should be equal after creation.');
+    test.done();
+  },
+  'a student has a list of previously asked questions': function (test) {
+    test.expect(2);
+    var student = new Student(3);
+    test.ok(student, 'should create a student without problem.');
+    test.equals(student.getAskedQuestions().length, 0, 'should have 0 questions asked.');
   }
 
 };
