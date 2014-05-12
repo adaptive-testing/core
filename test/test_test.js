f22dfde8379ecd643809fee26ad5002b9eb71c50
Clay Diffrient <clay.diffrient@gmail.com>
2014-05-11 23:55:23 -0600

    Fixes problems with JSHint   - Underscore not being defined   - No leading zero with decimal number.

--- test/test_test.js
+++ test/test_test.js
@@ -58,7 +58,7 @@ exports.TestModule = {
     test.expect(1);
     this.test.questions[0].difficulty = 1.5;
     this.test.questions[1].difficulty = 2.0;
-    this.test.questions[2].difficulty = .5;
+    this.test.questions[2].difficulty = 0.5;
     this.test.questions[3].difficulty = 1.0;
     var question = this.test.getNextQuestion();
     test.equals(question.text, 'Different', "Question text does not match properly.");
