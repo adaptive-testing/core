cecaa772bfd5809520e67c74c19393bd61ad42f4
Clay Diffrient <cdiffrient@instructure.com>
2014-09-30 23:57:29 -0600

    Adds the updated question model
    - Bumps the version to 0.0.1
    - Modifies npm test to only run Jasmine specs

---
+++ spec/models/QuestionSpec.js
@@ -0,0 +1,111 @@
+/**
+ * Tests for the Question model
+ */
+
+var Question = require('../../lib/models/Question');
+var mongoose = require('mongoose');
+var question;
+
+describe('Question Unit Tests', function () {
+  beforeEach(function () {
+    mongoose.connect(require('../../config/test.env').db);
+    question = new Question({
+      text: 'What is the right way to test?',
+      answers: [{
+        text: 'With lots of methods',
+        correct: false
+      },{
+        text: 'When it makes sense',
+        correct: true
+      }],
+      difficulty: 10,
+    });
+  });
+
+  afterEach(function () {
+    Question.remove().exec();
+    mongoose.disconnect();
+  });
+
+  it('should save the model without problems', function (done) {
+    question.save(function (err) {
+      expect(err).toBeNull();
+      done();
+    });
+  });
+
+  it('should not allow saving without text', function (done) {
+    question.text = '';
+    question.save(function (err) {
+      expect(err).toBeDefined();
+      done();
+    });
+  });
+
+  it('should not allow more than one correct answer', function (done) {
+    question.answers.push({text: 'With care', correct: true});
+    question.save(function (err) {
+      console.log(err);
+      expect(err.name).toBe('ValidationError');
+      done();
+    });
+  });
+
+  it('should not allow no answers', function (done) {
+    question.answers = [];
+    question.save(function (err) {
+      console.log(err);
+      expect(err.name).toBe('ValidationError');
+      done();
+    });
+  });
+
+  it('should only accept an array of answers', function (done) {
+    question.answers = "Cool answer";
+    question.save(function (err) {
+      console.log(err);
+      expect(err.name).toBe('TypeError');
+      done();
+    });
+  });
+
+  it('should not accept strings as difficulty', function (done){
+    question.difficulty = "Hard";
+    question.save(function (err) {
+      console.log(err);
+      expect(err.name).toBe('CastError');
+      done();
+    });
+  });
+
+  it('should not accept a difficulty less than zero (0)', function (done) {
+    question.difficulty = -1;
+    question.save(function (err) {
+      console.log(err);
+      expect(err.name).toBe('ValidationError');
+      done();
+    });
+  });
+
+  it('should not accept a difficulty more than one hundred (100)', function (done) {
+    question.difficulty = 101;
+    question.save(function (err) {
+      console.log(err);
+      expect(err.name).toBe('ValidationError');
+      done();
+    });
+  });
+
+
+
+  it('should return a proper value for the incorrect property', function (done) {
+    question.asked = 10;
+    question.correct = 5;
+    question.save(function (err, quest) {
+      expect(err).toBeNull();
+      expect(quest.incorrect).toBe(5);
+      done();
+    });
+  });
+
+});
--- spec/models/UserSpec.js
+++ spec/models/UserSpec.js
@@ -3,34 +3,49 @@
  */
 
 var User = require('../../lib/models/User');
+var mongoose = require('mongoose');
 
 var user;
+var originalTimeout;
 
 describe('User Unit Tests', function () {
 
-  beforeEach(function (){
+  // before(function () {
+  //   mongoose.connect(require('../../config/test.env').db);
+  // });
+
+  // after(function () {
+  //   mongoose.disconnect();
+  // });
+
+  beforeEach(function () {
+    mongoose.connect(require('../../config/test.env').db);
     user = new User({
       userId: 'testId',
       fullName: 'Testy McTester',
       email: 'testing@test.com',
       roles: 'student'
     });
+
   });
 
   afterEach(function () {
     User.remove().exec();
+    mongoose.disconnect();
   });
 
-  it('should save the model without problems', function () {
+  it('should save the model without problems', function (done) {
     user.save(function (err) {
-      expect(err).toBeUndefined();
-    })
+      expect(err).toBeNull();
+      done();
+    });
   });
 
-  it('should not allow saving without a user id', function () {
+  it('should not allow saving without a user id', function (done) {
     user.userId = '';
     user.save(function (err) {
       expect(err).toBeDefined();
+      done();
     });
   });
 
